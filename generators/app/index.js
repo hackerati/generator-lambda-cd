3af40a0630f753b0c4cce2a7df2627702d9b95f7
Bobby Yankou <thisisjackolantern@gmail.com>
2017-09-06 15:51:56 -0400

    Add code to create repo, hook travis, and trigger deployment

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,15 +7,28 @@ module.exports = class extends Generator {
     // Have Yeoman greet the user.
     this.log(yosay(`Welcome to the flawless ${chalk.red('generator-lambda-cd')} generator!`));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true,
-    }];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'sampleText',
+        message: 'What should the sample text be?',
+        default: 'Hello',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'AWS Access Key ID:',
+        default: 'Hello',
+      },
+      {
+        type: 'password',
+        name: 'secret',
+        message: 'AWS Secret Access Key:',
+        default: 'Hello',
+      },
+    ];
 
     return this.prompt(prompts).then((props) => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
@@ -24,11 +37,33 @@ module.exports = class extends Generator {
     this.fs.copyTpl(
       this.templatePath('{,.,**/}*'),
       this.destinationPath('.'),
-      {},
+      this.props,
     );
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({ bower: false });
+  }
+
+  end() {
+    // Initialize local repo
+    this.spawnCommandSync('git', ['init']);
+    this.spawnCommandSync('git', ['add', '.']);
+    this.spawnCommandSync('git', ['commit', '-m', '"Initial commit"']);
+    // Create remote remote repo and push
+    this.spawnCommandSync('hub', ['create']);
+    this.spawnCommandSync('git', ['push', 'origin', 'master']);
+    // Hook travis
+    const { status: loggedIn } = this.spawnCommandSync('travis', ['whoami']);
+    if (loggedIn !== 0) {
+      this.spawnCommandSync('travis', ['login']);
+    }
+    this.spawnCommandSync('travis', ['sync']);
+    this.spawnCommandSync('travis', ['enable']);
+    // Add AWS credentials to .travis.yml
+    this.spawnCommandSync('travis', ['encrypt', `AWS_ACCESS_KEY_ID=${this.props.id}`, `AWS_SECRET_ACCESS_KEY=${this.props.secret}`, '--override', '--add', 'env.matrix']);
+    // Trigger travis by pushing updated .travis.yml
+    this.spawnCommandSync('git', ['commit', '-am', '"update .travis.yml"']);
+    this.spawnCommandSync('git', ['push', 'origin', 'master']);
   }
 };
