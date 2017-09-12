a1ecd6e22fa72ed3932fe087a829ceede5bf6a0c
Nenad Samardzic <samardzicnenad@gmail.com>
2017-09-12 12:27:09 -0400

    NS/BY: Using user's GitHub credentials (and two factor authentication code) to get the GitHub token

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,7 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const requestPromise = require('request-promise');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -18,25 +19,93 @@ module.exports = class extends Generator {
         type: 'input',
         name: 'sampleText',
         message: 'What should the sample text be?',
-        default: 'Hello',
+        default: 'Hello Lambda',
       },
       {
         type: 'input',
         name: 'id',
-        message: 'AWS Access Key ID:',
+        message: 'Provide your AWS Access Key ID:',
         default: 'Hello',
       },
       {
         type: 'password',
         name: 'secret',
-        message: 'AWS Secret Access Key:',
+        message: 'Provide your AWS Secret Access Key:',
         default: 'Hello',
+      },
+      {
+        type: 'input',
+        name: 'repoName',
+        message: 'What should the name of the GitHub repository be?',
+        default: 'my-lambda-cd',
+      },
+      {
+        type: 'input',
+        name: 'githubUser',
+        message: 'Provide your GitHub username:',
+        default: 'username',
+      },
+      {
+        type: 'password',
+        name: 'githubPassword',
+        message: 'Provide your GitHub password:',
+        default: 'password',
       },
     ];
 
-    return this.prompt(prompts).then((props) => {
-      this.props = props;
-    });
+    return this.prompt(prompts)
+      .then((props) => {
+        const dataString = `{"scopes": ["repo", "user"], "note": "getting-started-at-${Date.now()}"}`;
+        const options = {
+          url: 'https://api.github.com/authorizations',
+          headers: {
+            'User-Agent': props.githubUser,
+          },
+          method: 'POST',
+          body: dataString,
+          resolveWithFullResponse: true,
+          auth: {
+            user: props.githubUser,
+            pass: props.githubPassword,
+          },
+        };
+
+        return requestPromise(options)
+          .then((response) => {
+            this.props = props;
+            this.props.token = JSON.parse(response.body).token;
+            return true;
+          })
+          .catch((error) => {
+            // There is a two-factor authentication enabled; Ask for the authentication code
+            if (error.statusCode === 401) {
+              const authenticationCodePrompt = [
+                {
+                  type: 'input',
+                  name: 'githubAuthCode',
+                  message: 'Provide your GitHub authentication code:',
+                  default: 'Hello',
+                },
+              ];
+
+              return this.prompt(authenticationCodePrompt)
+                .then((githubProp) => {
+                  options.headers['X-GitHub-OTP'] = githubProp.githubAuthCode;
+
+                  return requestPromise(options)
+                    .then((authenticationCodeResponse) => {
+                      this.props = props;
+                      this.props.token = JSON.parse(authenticationCodeResponse.body).token;
+                      return true;
+                    })
+                    .catch(() => {
+                      return false;
+                    });
+                });
+            }
+            return false;
+          });
+      });
   }
 
   writing() {
